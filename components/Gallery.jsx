b2931e5d5154eab947bdd7f0b5ac028317a8513a
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2024-10-02 18:28:14 -0300

    Update Gallery.jsx

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -28,21 +28,28 @@ const Gallery = () => {
     { src: img1, srctxt: '../src/assets/Concursos/Concurso_Urbano_Imagina_Bolivar/1.webp', text: "CONCURSO Urbano Imagina Bolivar" },
     { src: img2, srctxt: '../src/assets/Concursos/Concurso_Biblioteca_Sarmiento/1.webp', text: "CONCURSO Biblioteca Sarmiento" },
     { src: portadaCosta, srctxt: '../src/assets/Concursos/Concurso_Costa_Urbana/1.webp', text: "CONCURSO Costa Urbana" },
-    { src: portadaHawa, srctxt: '../src/assets/Concursos/Concurso_Hawa/1.webp', text: "CONCURSO Hawa" },
-    { src: portadaSumInta, srctxt: '../src/assets/Concursos/Concurso_Sum_Inta/1.webp', text: "CONCURSO Sum Inta" },
+    { src: portadaHawa, srctxt: '../src/assets/Concursos/Concurso_HAWA/1.webp', text: "CONCURSO HAWA" },
+    { src: portadaSumInta, srctxt: '../src/assets/Concursos/Concurso_SUM_INTA/1.webp', text: "CONCURSO SUM INTA" },
     { src: portadaSantaFe, srctxt: '../src/assets/Viviendas/Santa_Fe/1.webp', text: "VIVIENDAS Santa Fe" },
     { src: portadaAlberti, srctxt: '../src/assets/Viviendas/Alberti/1.webp', text: "VIVIENDAS Alberti" },
     { src: portadaSanSebastian, srctxt: '../src/assets/Viviendas/San_Sebastian_A1/1.webp', text: "VIVIENDAS San Sebastian" },
-    { src: portadaSmata, srctxt: '../src/assets/Viviendas/Smata/1.webp', text: "VIVIENDAS Smata" },
+    { src: portadaSmata, srctxt: '../src/assets/Viviendas/SMATA/1.webp', text: "VIVIENDAS SMATA" },
     { src: portadaEstudioINA, srctxt: '../src/assets/Comercial/Estudio_INA/1.webp', text: "COMERCIAL Estudio INA" },
     { src: portadaDino, srctxt: '../src/assets/Comercial/DINO/1.webp', text: "COMERCIAL DINO" },
-    { src: portadaManuela, srctxt: '../src/assets/Reformas/Manuela_Pedraza/1.webp', text: "REFORMAS Manuela Pedraza" },
+    { src: portadaManuela, srctxt: '../src/assets/Reformas/Manuela_Pedraza/Renders/1.webp', text: "REFORMAS Manuela Pedraza" },
     { src: portadaTerrada, srctxt: '../src/assets/Reformas/Cocina_Terrada/1.webp', text: "REFORMAS Cocina Terrada" }
+    
   ];
 
   const handleLoadMore = () => {
     setVisibleCount((prevCount) => prevCount + 10);
   };
+  const handleClick = (image) => {
+    const folderName = image.srctxt.split('/')[3]; // Extraer el nombre de la carpeta dinámica
+    const projectName = image.srctxt.split('/')[4]; // Extraer el nombre del proyecto
+    navigate(`/${folderName}/${projectName}`);
+  };
+
 
   return (
     <div className="gallery">
@@ -53,7 +60,7 @@ const Gallery = () => {
             src={image.src}
             alt={`Imagen ${index + 1}`}
             text={image.text}
-            onClick={() => navigate("/concursos/" + image.srctxt.split('/')[4])}
+            onClick={() => handleClick(image)}
           />
         ))}
       </div>
