0dc68285c733a4b8c86560a853f4731a062d3906
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2024-10-02 17:34:14 -0300

    skeleton implementado

--- components/SkeletonCarrousel.jsx
+++ components/SkeletonCarrousel.jsx
@@ -3,10 +3,30 @@ import "../src/index.css";
 
 const SkeletonCarrousel = () => {
   return (
-    <div className="skeleton-carrousel">
-      <div className="skeleton-slide"></div>
-      <div className="skeleton-slide"></div>
-      <div className="skeleton-slide"></div>
+    <div className="carousel skeleton" aria-label="Loading Skeleton Carousel">
+      <div className="carousel-inner">
+        {/* Elemento que simula el contenido cargando */}
+        <div className="carousel-item">
+          <div className="skeleton-image"></div>
+        </div>
+        <div className="carousel-item">
+          <div className="skeleton-image"></div>
+        </div>
+        <div className="carousel-item">
+          <div className="skeleton-image"></div>
+        </div>
+      </div>
+      <button className="carousel-control-prev" aria-label="Previous Slide" disabled>
+        <span className="carousel-control-prev-icon"></span>
+      </button>
+      <button className="carousel-control-next" aria-label="Next Slide" disabled>
+        <span className="carousel-control-next-icon"></span>
+      </button>
+      <div className="carousel-thumbnails">
+        <div className="skeleton-thumbnail"></div>
+        <div className="skeleton-thumbnail"></div>
+        <div className="skeleton-thumbnail"></div>
+      </div>
     </div>
   );
 };
