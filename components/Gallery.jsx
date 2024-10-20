8acb8d50c8fff1181aff67c2d76a0b4fd351ba41
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2024-10-20 14:33:19 -0300

    gallery cambios 2

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -20,11 +20,13 @@ import portadaBilbao from "../src/assets/Comercial/3_Local_Francisco_Bilbao/1.we
 import portadaTrenesArgentinos from "../src/assets/Comercial/6_Trenes_Argentinos/Renders/1.webp";
 // import portadaPorteniaBiblioteca from "../src/assets/Viviendas/3_Porteña_Biblioteca/1.webp";
 // import portadaPorteniaLiving from "../src/assets/Viviendas/3_Porteña_Living/1.webp";
+// import portadaManuelaPedraza from "../src/assets/Reformas/1_Manuela_Pedraza/Renders/PORTADA.webp";
 import portadaDefensaBar from "../src/assets/Comercial/5_Defensa_Bar/1.webp";
 import portadaParqueSalguero from "../src/assets/Concursos/2_Concurso_Parque_Salguero/PORTADA.webp";
 import portadaOficinasSiconara from "../src/assets/Comercial/1_Oficinas_Siconara/Renders/1.webp";
 // import portadaReformaBeruti from "../src/assets/Reformas/3_Reforma_Beruti/1.webp";
-// import portadaConcursoParqueTecnologico from "../src/assets/Concursos/2_Concurso_Parque_Tecnologico/1.webp";
+import portadaConcursoParqueTecnologico from "../src/assets/Concursos/5_Concurso_Polo_y_Parque_Tecnologico/Renders/1.webp";
+import portadaConcursoEducacionDelFuturo from "../src/assets/Concursos/Concurso_Educacion_Del_Futuro/1.webp";
 
 const Gallery = () => {
   const navigate = useNavigate();
@@ -42,18 +44,18 @@ const Gallery = () => {
     { src: portadaTrenesArgentinos, srctxt: '../src/assets/Comercial/6_Trenes_Argentinos/Renders/1.webp', text: "COMERCIAL Trenes Argentinos" },
     // { src: portadaPorteniaBiblioteca, srctxt: '../src/assets/Viviendas/3_Porteña_Biblioteca/1.webp', text: "VIVIENDAS La Porteña Biblioteca" },
     // { src: portadaPorteniaLiving, srctxt: '../src/assets/Viviendas/3_Porteña_Living/1.webp', text: "VIVIENDAS La Porteña Living" },
-    // { src: portadaTerrada, srctxt: '../src/assets/Reformas/2_Terrada/1.webp', text: "REFORMAS Terrada" },
+    // { src: portadaManuelaPedraza, srctxt: '../src/assets/Reformas/1_Manuela_Pedraza/Renders/PORTADA.webp', text: "REFORMAS Manuela Pedraza" },
     { src: portadaEstudioINA, srctxt: '../src/assets/Comercial/7_Estudio_INA/1.webp', text: "COMERCIAL Estudio INA" },
     { src: portadaDefensaBar, srctxt: '../src/assets/Comercial/5_Defensa_Bar/1.webp', text: "COMERCIAL Defensa Bar" },
     { src: portadaParqueSalguero, srctxt: '../src/assets/Concursos/2_Concurso_Parque_Salguero/PORTADA.webp', text: "CONSURSOS Parque Salguero" },
-    {src:portadaOficinasSiconara , srctxt: '../src/assets/Comercial/1_Oficinas_Siconara/Renders/1.webp', text: "COMERCIAL Oficinas Siconara"},
+    { src: portadaOficinasSiconara, srctxt: '../src/assets/Comercial/1_Oficinas_Siconara/Renders/1.webp', text: "COMERCIAL Oficinas Siconara" },
     { src: portadaSmata, srctxt: '../src/assets/Viviendas/9_SMATA/1.webp', text: "VIVIENDAS SMATA" },
     { src: portadaCosta, srctxt: '../src/assets/Concursos/3_Concurso_Costa_Urbana/1.webp', text: "CONCURSO Costa Urbana" },
     // {src:portadaReformaBeruti , srctxt: '../src/assets/Reformas/3_Reforma_Beruti/1.webp', text: "REFORMAS Beruti"},
     { src: portadaAlberti, srctxt: '../src/assets/Viviendas/7_Alberti/1.webp', text: "VIVIENDAS Alberti" },
-    // {src:portadaConcursoParqueTecnologico , srctxt: '../src/assets/Concursos/2_Concurso_Parque_Tecnologico/1.webp', text: "CONCURSO Parque Tecnologico"},
+    { src: portadaConcursoParqueTecnologico, srctxt: '../src/assets/Concursos/5_Concurso_Polo_y_Parque_Tecnologico/Renders/1.webp', text: "CONCURSO Parque y Polo Tecnologico Acindar" },
     { src: portadaSumInta, srctxt: '../src/assets/Concursos/6_Concurso_SUM_INTA/1.webp', text: "CONCURSO SUM INTA" },
-    // {src:portadaConcursoParqueTecnologico , srctxt: '../src/assets/Concursos/2_Concurso_Parque_Tecnologico/1.webp', text: "CONCURSO Parque Tecnologico"},
+    {src:portadaConcursoEducacionDelFuturo , srctxt: '../src/assets/Concursos/Concurso_Educacion_Del_Futuro/1.webp', text: "CONCURSO Educación del Futuro"},
   ];
 
   const handleLoadMore = () => {
