1ae51eacc558d2a0d96b3b798ec1b1f3b169a786
Agustin Chizzini Melo <aguscn2001@gmail.com>
2024-10-22 12:16:38 -0300

    gallery changes

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -9,18 +9,18 @@ import img1 from '../src/assets/Concursos/1_Concurso_Urbano_Imagina_Bolivar/1.we
 import portadaCosta from "../src/assets/Concursos/3_Concurso_Costa_Urbana/1.webp";
 import portadaHawa from "../src/assets/Concursos/4_Concurso_HAWA/1.webp";
 import portadaSumInta from "../src/assets/Concursos/6_Concurso_SUM_INTA/1.webp";
-import portadaSantaFe from "../src/assets/Viviendas/5_Santa_Fe/Renders/1.webp";
+import portadaSantaFe from "../src/assets/Reformas/3_Santa _Fe/Renders/1.webp";
 import portadaAlberti from "../src/assets/Viviendas/7_Alberti/1.webp";
-// import portadaSmata from "../src/assets/Viviendas/9_SMATA/PORTADA.webp";
+import portadaSmata from "../src/assets/Viviendas/9_SMATA/Renders/1.webp";
 import portadaEstudioINA from "../src/assets/Comercial/7_Estudio_INA/1.webp";
 import portadaBgo from "../src/assets/Comercial/4_Bauness/1.webp";
 import portadaTerrada from "../src/assets/Reformas/2_Cocina_Terrada/Renders/1.webp";
 import portadaBilbao from "../src/assets/Comercial/3_Local_Francisco_Bilbao/1.webp";
-// import portadaPorteniaBar from "../src/assets/Viviendas/3_Porteña_Bar/1.webp";
+import portadaPorteniaBar from "../src/assets/Viviendas/1_Faena_La_Porteña_Bar/Renders/1.webp";
 import portadaTrenesArgentinos from "../src/assets/Comercial/6_Trenes_Argentinos/Renders/1.webp";
-// import portadaPorteniaBiblioteca from "../src/assets/Viviendas/3_Porteña_Biblioteca/1.webp";
-// import portadaPorteniaLiving from "../src/assets/Viviendas/3_Porteña_Living/1.webp";
-// import portadaManuelaPedraza from "../src/assets/Reformas/1_Manuela_Pedraza/Renders/PORTADA.webp";
+import portadaPorteniaBiblioteca from "../src/assets/Viviendas/3_Faena_La_Porteña_Biblioteca/Renders/1.webp";
+import portadaPorteniaLiving from "../src/assets/Viviendas/2_Faena_La_Porteña_Living/Renders/1.webp";
+import portadaManuelaPedraza from "../src/assets/Reformas/1_Manuela_Pedraza/Renders/1.webp";
 import portadaDefensaBar from "../src/assets/Comercial/5_Defensa_Bar/1.webp";
 import portadaParqueSalguero from "../src/assets/Concursos/2_Concurso_Parque_Salguero/PORTADA.webp";
 import portadaOficinasSiconara from "../src/assets/Comercial/1_Oficinas_Siconara/Renders/1.webp";
@@ -36,20 +36,20 @@ const Gallery = () => {
   const images = [
     { src: img1, srctxt: '../src/assets/Concursos/1_Concurso_Urbano_Imagina_Bolivar/1.webp', text: "CONCURSO Urbano Imagina Bolivar" },
     { src: portadaBgo, srctxt: '../src/assets/Comercial/4_Bauness/1.webp', text: "COMERCIAL BGO" },
-    { src: portadaSantaFe, srctxt: '../src/assets/Viviendas/5_Santa_Fe/Renders/1.webp', text: "VIVIENDAS Santa Fe" },
+    { src: portadaSantaFe, srctxt: '../src/assets/Reformas/3_Santa _Fe/Renders/1.webp', text: "REFORMAS Santa Fe" },
     { src: portadaHawa, srctxt: '../src/assets/Concursos/4_Concurso_HAWA/1.webp', text: "CONCURSO HAWA" },
     { src: portadaBilbao, srctxt: '../src/assets/Comercial/3_Local_Francisco_Bilbao/1.webp', text: "COMERCIAL Bilbao" },
     { src: portadaTerrada, srctxt: '../src/assets/Reformas/2_Cocina_Terrada/1.webp', text: "REFORMAS Cocina Terrada" },
-    // { src: portadaPorteniaBar, srctxt: '../src/assets/Viviendas/3_Porteña_Bar/1.webp', text: "VIVIENDAS La Porteña Bar" },
+    { src: portadaPorteniaBar, srctxt: '../src/assets/Viviendas/1_Faena_La_Porteña_Bar/Renders/1.webp', text: "VIVIENDAS La Porteña Bar" },
     { src: portadaTrenesArgentinos, srctxt: '../src/assets/Comercial/6_Trenes_Argentinos/Renders/1.webp', text: "COMERCIAL Trenes Argentinos" },
-    // { src: portadaPorteniaBiblioteca, srctxt: '../src/assets/Viviendas/3_Porteña_Biblioteca/1.webp', text: "VIVIENDAS La Porteña Biblioteca" },
-    // { src: portadaPorteniaLiving, srctxt: '../src/assets/Viviendas/3_Porteña_Living/1.webp', text: "VIVIENDAS La Porteña Living" },
-    // { src: portadaManuelaPedraza, srctxt: '../src/assets/Reformas/1_Manuela_Pedraza/Renders/PORTADA.webp', text: "REFORMAS Manuela Pedraza" },
+    { src: portadaPorteniaBiblioteca, srctxt: '../src/assets/Viviendas/3_Faena_La_Porteña_Biblioteca/Renders/1.webp', text: "VIVIENDAS La Porteña Biblioteca" },
+    { src: portadaPorteniaLiving, srctxt: '../src/assets/Viviendas/2_Faena_La_Porteña_Living/Renders/1.webp', text: "VIVIENDAS La Porteña Living" },
+    { src: portadaManuelaPedraza, srctxt: '../src/assets/Reformas/1_Manuela_Pedraza/Renders/1.webp', text: "REFORMAS Manuela Pedraza" },
     { src: portadaEstudioINA, srctxt: '../src/assets/Comercial/7_Estudio_INA/1.webp', text: "COMERCIAL Estudio INA" },
     { src: portadaDefensaBar, srctxt: '../src/assets/Comercial/5_Defensa_Bar/1.webp', text: "COMERCIAL Defensa Bar" },
     { src: portadaParqueSalguero, srctxt: '../src/assets/Concursos/2_Concurso_Parque_Salguero/PORTADA.webp', text: "CONSURSOS Parque Salguero" },
     { src: portadaOficinasSiconara, srctxt: '../src/assets/Comercial/1_Oficinas_Siconara/Renders/1.webp', text: "COMERCIAL Oficinas Siconara" },
-    // { src: portadaSmata, srctxt: '../src/assets/Viviendas/9_SMATA/PORTADA.webp', text: "VIVIENDAS SMATA" },
+    { src: portadaSmata, srctxt: '../src/assets/Viviendas/9_SMATA/Renders/1.webp', text: "VIVIENDAS SMATA" },
     { src: portadaCosta, srctxt: '../src/assets/Concursos/3_Concurso_Costa_Urbana/1.webp', text: "CONCURSO Costa Urbana" },
     // {src:portadaReformaBeruti , srctxt: '../src/assets/Reformas/3_Reforma_Beruti/1.webp', text: "REFORMAS Beruti"},
     { src: portadaAlberti, srctxt: '../src/assets/Viviendas/7_Alberti/1.webp', text: "VIVIENDAS Alberti" },
