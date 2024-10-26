d1bbd0bc8a34673f014c5939a02dfdc6909afa4e
Agustin Chizzini Melo <57108480+chizzi01@users.noreply.github.com>
2024-10-26 10:38:05 -0300

    Update Gallery.jsx

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -98,7 +98,7 @@ const LazyImage = ({ src, alt, text, onClick }) => {
   });
 
 
-  console.log("Nuevo cambiooooooooooooooooooooooooooooooo");
+  console.log("cambio miche miche michedasadasdsads");
 
   return (
     <div className="gallery-item" onClick={onClick} style={{ cursor: 'pointer' }} ref={ref}>
